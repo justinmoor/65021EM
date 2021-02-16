bf9d2ce458d2ddb33796eee3cdefda5a7d2cbc58
justinmoor <s1101630@student.hsleiden.nl>
2021-02-16 22:11:15 +0100

    initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# 65021ME
+65021ME stands for 65021 Expandable Microcomputer, a 6502-based computer, made in 2021. This repository is holds all the resources necessary for the project, which includes source code and schematics.
+
+### Specifications
+
+- **CPU:** 65C02
+- **RAM**: 32KB
+- **ROM**: 16KB
+
+### Features
+
+- Serial interface 
+- SPI interface (todo)
+
+### Memory Map
+
+**RAM**: \$0000 - \$7FFF (binary 0xxxxxxxxxxxxxxx)
+**ROM**: \$C000 - \$FFFF (binary 11xxxxxxxxxxxxxx)
+**SERIAL**: \$8000 - \$800F (binary 1000000000001111)
+
+### Parts list
+- 65C02
---
+++ src/kernel.asm
@@ -0,0 +1,101 @@
+    .setcpu "65C02"
+
+    .segment "VECTORS"
+
+    .word   NMI
+    .word   RESET
+    .word   IRQ
+
+    .code
+
+    VIA_DATAB = $8000
+    VIA_DATAA = $8001
+
+    VIA_DDRB = $8002
+    VIA_DDRA = $8003
+
+    max3100ReadBuffer = $30
+    spiWriteBuffer = $20
+    spiReadBuffer = $10
+
+RESET:  JMP INIT_VIA
+NMI:    RTI
+IRQ:    RTI
+
+INIT_VIA:
+    LDX #$FF        ; setup stack
+    TXS        
+    LDA #%00000011  ; configure the MOSI and CLK pin as outputs, others as inputs
+    STA VIA_DDRB
+    LDA #$FF
+    STA VIA_DDRA    ; configure all pins as outputs, 8 slave selects available
+    STA VIA_DATAA   ; set all to high
+
+INIT_MAX3100:
+    ; MAX3100 Config: 11000000 00001010
+    LDY #%11000000    
+    LDA #%00001010  ; 9600 baud
+    JSR WRITE_MAX
+
+    LDX #0
+LOOP:
+    LDA text,x
+    BEQ STOP
+    JSR WRITE_CHAR
+    INX
+    JMP LOOP
+
+STOP:
+    NOP
+    NOP
+    JMP STOP    
+
+WRITE_CHAR:
+WAIT:
+    PHA
+    LDY #%01000000
+    LDA #%00000000
+    JSR WRITE_MAX
+    LDA max3100ReadBuffer
+    AND #%01000000
+    BEQ WAIT
+    PLA
+    LDY #%10000000
+; writes a 16 bit sequence to the MAX3100
+; assumes the command is stored in the Y register and the
+; actual data in the A register
+WRITE_MAX:
+    PHA
+    TYA
+    STZ VIA_DATAA           ; select MAX3100
+    JSR SPI_WRITE_BYTE
+    STA max3100ReadBuffer
+    PLA
+    JSR SPI_WRITE_BYTE
+    STA max3100ReadBuffer + 1
+    LDA #$FF
+    STA VIA_DATAA           ; deselect MAX3100
+    RTS
+
+SPI_WRITE_BYTE:
+    STA spiWriteBuffer
+    LDY #$8                 ; write 8 bits
+WRITE_BIT:
+    LDA #%0                 ; zero bit the output line
+    ROL spiWriteBuffer         ; rotate the buffer so the bit to be written is represented by the carry flag
+    BCC WRITE               ; 0 in carry flag? continue to writing the zero bit
+    ORA #%00000010          ; 1 in carry flag, set MOSI to high
+WRITE:
+    STA VIA_DATAB           ; write bit
+    INC VIA_DATAB           ; set clock high
+    LDA VIA_DATAB           ; read bit
+    ROL                     ; just read bit is represented in PB7, rotate it into carry
+    ROL spiReadBuffer          ; rotate bit from carry into spiReadBuffer
+    DEC VIA_DATAB           ; set clock low
+    DEY
+    BNE WRITE_BIT
+    LDA spiReadBuffer
+    RTS
+
+text:
+    .asciiz "welcome"
